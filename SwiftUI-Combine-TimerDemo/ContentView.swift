2e23d878870d54923a70e9aae7621687a46c5e9b
russell-archer <russell.archer@mac.com>
2019-08-24 20:13:27 +0100

    Initial Commit

---
+++ SwiftUI-Combine-TimerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine-TimerDemo
+//
+//  Created by Russell Archer on 24/08/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
